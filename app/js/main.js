baf7a6adcc716844dcbe2821667c357344992aef
Aleksey <alexmatusevich.97@gmail.com>
2022-03-10 12:22:13 +0300

    page contact end

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,12 @@ $(function (){
         fade:true,
         autoplay:true
     });
-
+    $('.contact__slider').slick({
+        slidesToScroll: 10,
+        slidesToShow:10,
+        dots: true,
+        arrows:false,
+    });
     var mixer = mixitup('.gallery__inner',{
         load:{
             filter:'.living'
